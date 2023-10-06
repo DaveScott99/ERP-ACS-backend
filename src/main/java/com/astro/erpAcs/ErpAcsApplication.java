4b0b0f622b866f90c807265d49766c62c9349a52
DaviSantos <davicalixto2077@gmail.com>
2023-10-06 14:45:37 -0300

    Project created

---
+++ src/main/java/com/astro/erpAcs/ErpAcsApplication.java
@@ -0,0 +1,13 @@
+package com.astro.erpAcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astro/erpAcs/ErpAcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.erpAcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
